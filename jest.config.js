4ef3471b69acfca9b89bcad43062489e8801fd94
manassevisser <manassevisser@gmail.com>
2025-12-27 03:29:14 +0000

    chore: phoenix stabilized — grade A+ build achieved

--- jest.config.js
+++ jest.config.js
@@ -9,21 +9,119 @@ module.exports = {
 
   /* @aliases-start */
 moduleNameMapper: {
-  '^\@domain/(.*)$': '<rootDir>/src/domain/$1',
-  '^\@state/(.*)$': '<rootDir>/src/state/$1',
-  '^\@ui/(.*)$': '<rootDir>/src/ui/$1',
-  '^\@styles/(.*)$': '<rootDir>/src/ui/styles/$1',
-  '^\@app/(.*)$': '<rootDir>/src/app/$1',
-  '^\@utils/(.*)$': '<rootDir>/src/utils/$1',
-  '^\@services/(.*)$': '<rootDir>/src/services/$1',
-  '^\@assets/(.*)$': '<rootDir>/assets/$1',
-  '^\@logic/(.*)$': '<rootDir>/src/logic/$1',
-  '^\@config/(.*)$': '<rootDir>/src/config/$1',
-  '^\@context/(.*)$': '<rootDir>/src/app/context/$1',
-  '^\@selectors/(.*)$': '<rootDir>/src/selectors/$1',
-  '^\@shared-types/(.*)$': '<rootDir>/src/shared-types/$1',
-  '^\@components/(.*)$': '<rootDir>/src/ui/components/$1',
-},
+    '^\@domain/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/domain/$1',
+    '^\@state/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/state/$1',
+    '^\@ui/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/ui/$1',
+    '^\@styles/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/ui/styles/$1',
+    '^\@app/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/app/$1',
+    '^\@utils/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/utils/$1',
+    '^\@services/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/services/$1',
+    '^\@assets/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/_assets/$1',
+    '^\@logic/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/logic/$1',
+    '^\@config/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/config/$1',
+    '^\@context/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/app/context/$1',
+    '^\@selectors/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/selectors/$1',
+    '^\@shared-types/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/shared-types/$1',
+    '^\@components/(.*),
+/* @aliases-end */
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community)',
+  ],
+};
+: '<rootDir>/src/ui/components/$1',
+  },
 /* @aliases-end */
 
   transformIgnorePatterns: [
